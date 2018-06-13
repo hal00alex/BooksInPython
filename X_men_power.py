17d6151bf9e73d2b25ec1a60b5da8f14641d81c6
Haley Alexander <15005204+hal00alex@users.noreply.github.com>
2018-06-13 08:05:54 -0500

    Create X_men_power.py

---
+++ X_men_power.py
@@ -0,0 +1,8 @@
+import os
+import random
+def main():
+  list_1 = ["Flight","teleporting","Portals","Energy Field","Metal","Ice","Diamond","Shape-shifter","Energy Absorbtion","Energy Blasts","Lasers","Telekintic","Telepathic","Healing","explosions","Speed","Fire","Earthquakes","Illusions","Senses","Phase Shifting","Magentic", "Acid", "Momentum"]
+  index = random.randint(0,(len(list_1)-1))
+  print "Your power is \n"
+  print (list_1[index])
+main()
