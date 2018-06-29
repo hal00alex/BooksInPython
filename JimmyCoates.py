8ea8634f97ad39be3cc3eff78b28f7f1bbd3af16
Haley Alexander <15005204+hal00alex@users.noreply.github.com>
2018-06-29 12:12:31 -0500

    Create JimmyCoates.py

---
+++ JimmyCoates.py
@@ -0,0 +1,12 @@
+import random
+def main():
+  print "Hello Master\n"
+  jimmyAssassin = ""
+  index = random.randint(0,100)
+  if (index < 30):
+    jimmyAssassin = "Yes"
+  else:
+    jimmyAssassin = "No"
+  print (jimmyAssassin)
+  print "\nGoodbye Master\n"
+main()
