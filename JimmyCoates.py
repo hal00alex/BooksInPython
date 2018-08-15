6a4a9e8097fdfb4ce54d62c9cd51c281fbeee75b
Haley Alexander <15005204+hal00alex@users.noreply.github.com>
2018-08-15 13:09:32 -0500

    Update JimmyCoates.py

--- JimmyCoates.py
+++ JimmyCoates.py
@@ -3,6 +3,8 @@ def main():
   print "Hello Master\n"
   jimmyAssassin = ""
   index = random.randint(0,100)
+  #based on the books. He has a 30 % to ignore his program completely
+  #change 30 to a higher number if you want to include the times when he ignores his program paritially.
   if (index < 30):
     jimmyAssassin = "Yes"
   else:
